87e8d7b59a40cfae0efb9e917a372d629a101eaa
庄新凯 <zhuanxinkai.wh@haier.com>
2020-01-02 17:43:35 +0800

    feat: 首页，关于

--- themes/toko/scripts/posts.js
+++ themes/toko/scripts/posts.js
@@ -1,9 +1,13 @@
 hexo.extend.generator.register('test', function (locals) {
     const config = this.config;
-    return {
+    return [{
         path: 'posts/index.html',
         data: locals.posts,
         layout: ['posts', 'index']
-    };
+    },{
+        path: 'about/index.html',
+        data: {},
+        layout: ['about', 'index'] 
+    }];
 
 });
